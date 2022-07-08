5e2f65335c858ef52f6e11d92fd6b27c8492060b
saqib nazir <saqib_9090@yahoo.com>
2022-07-08 14:49:43 +0100

    displaying tours

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
-const Tours = () => {
-  return <div>Tours</div>;
+import Tour from './Tour';
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour}></Tour>;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
